603bcb6824ae8854202cd355dc3be8b3b6cbae63
penguin-one <ilya-doroshevich@yandex.ru>
2023-02-24 22:49:30 +0300

    feat: add a solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,29 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+        this.arrRange = [];
 
+        for (let i = min; i <= max; i++) {
+            this.arrRange.push(i);
+        }
+
+        this.left = 0;
+        this.right = this.arrRange.length - 1;
+        this.mid;
     }
 
     guess() {
+        this.mid = Math.round( (this.right + this.left) / 2 );
 
+        return this.arrRange[this.mid];
     }
 
     lower() {
-
+        this.right = this.mid;
     }
 
     greater() {
-
+        this.left = this.mid;
     }
 }
 
